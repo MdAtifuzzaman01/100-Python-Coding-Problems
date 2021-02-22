19624c67f3d346f63a9cd3268c37453d9e105f8e
Md Atifuzzaman <76816638+MdAtifuzzaman01@users.noreply.github.com>
2021-02-22 11:51:44 +0600

    Add files via upload

---
+++ Computations/Complex-Interest.py
@@ -0,0 +1,12 @@
+# We are gonna take money borrowed, interest and duration as input. Then, compute the compound interest rate.
+def compound_interest(principle,rate,time):
+    interest = principle * ((1 + rate / 100) ** time)
+    return interest
+principle = int(input("Money you borrowed : "))
+interest_rate = float(input("Interest Rate(% not needed) : "))
+time = float(input("Overall Duration(year) : "))
+
+total_due = compound_interest(principle, interest_rate, time)
+
+print("Interest amount is : " , total_due)
+
